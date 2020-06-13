826f6954d118e17c625acf3180bc8054e2504530
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-06-13 12:25:20 +0200

    save

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      name = @message.name
+      email = @message.email
+      subject = @message.subject
+      content = @message.content
+      OwnerMailer.message_received(name, email, subject, content).deliver_now
+      redirect_to new_message_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :subject, :content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_13_100942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "subject"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
