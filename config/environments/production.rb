02fe5c43d15f5d02b24581ff5e4b34a24ebf512a
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-06-14 16:22:39 +0200

    banner on all devices

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "mighty-castle-85679.herokuapp.com" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -28,8 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "mighty-castle-85679.herokuapp.com" }
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
